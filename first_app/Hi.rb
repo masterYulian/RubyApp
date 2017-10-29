59453f79734bb026e34e81813fa3fa37c101c089
masterYulian <yulian.mylyan@nltu.lviv.ua>
2017-10-29 22:54:48 +0200

    Some change

---
+++ first_app/Hi.rb
@@ -0,0 +1,12 @@
+# require 'pry'
+
+puts 'Hello ;)'
+puts('Hi hi ;)')
+puts(2 + 3 * 4)
+puts('Changing')
+puts('Changing')
+a = [1]
+a << '2'
+a << {name: 'Oleh', age: 12}
+# binding.pry
+p a
--- temp/Hi.rb
+++
@@ -1,5 +0,0 @@
-puts('Hello ;)')
-puts('Hi hi ;)')
-puts(2 + 3 * 4)
-puts('Changing')
-puts('Changing')
