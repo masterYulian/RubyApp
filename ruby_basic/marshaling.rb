67cf14db721976e327e7d6d99ce498f82c5e0c88
masterYulian <yulian.mylyan@nltu.lviv.ua>
2017-12-24 23:15:03 +0200

    Update

--- ruby_basic/marshaling.rb
+++ ruby_basic/marshaling.rb
@@ -1,2 +1,24 @@
 class Marshaling
 end
+class Player
+  attr_accessor :name, :progress
+
+  def initialize(name)
+    @name = name
+  end
+
+  def to_s
+    object = 'Name = ' + @name
+  end
+end
+
+player = Player.new("Jason")
+player.progress = 60
+
+player_out = Marshal.dump(player)
+
+puts player_out.inspect
+
+player_in = Marshal.load(player_out)
+
+puts player_in
