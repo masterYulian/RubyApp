37f15110585adec3ba2e0c7f0601fa51100cedec
masterYulian <yulian.mylyan@nltu.lviv.ua>
2018-01-03 12:40:11 +0200

    Add e-learning

---
+++ calculator.rb
@@ -0,0 +1,10 @@
+﻿class Calculator
+  def hash_key_criteria input_hash
+    temp = input_hash
+    result = temp.select { |key, value| key.to_s.length > 3 }
+    result =result.keys
+    puts result
+  end
+end
+calculator = Calculator.new
+calculator.hash_key_criteria({"ap" => "fruit", "carrot" => "vegetable"})
